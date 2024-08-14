e7886ce3772fbda3d4066945d2fc9ad2926ea558
MARCO DI CARLO <48115731@ort>
2024-08-14 11:20:55 -0300

    Layout finalizado

--- obj/Debug/net8.0/TP_JJOO.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP_JJOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cacbe56bc2ecc87a1803e6d4d79995de9edc0689")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf0c5250c54548207b003d3fe40924d8f40f665")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
