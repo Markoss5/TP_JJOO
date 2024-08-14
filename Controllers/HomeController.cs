b7fb4946d167f330720ef7c907569adf55bf46df
Iamatoast <neuyv@protonmail.com>
2024-08-14 12:12:43 -0300

    Controllers hechos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,62 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Deportes()
+    {
+        ViewBag.DeportesLista = BD.ListarDeportes();
+        return View();
+    }
+
+    public IActionResult Paises()
+    {
+        ViewBag.PaisesList = BD.ListarPaises();
+        return View();
+    }
+
+    public IActionResult VerDetalleDeporte(int idDeporte)
+    {
+        ViewBag.Deporte = BD.VerInfoDeporte(idDeporte);
+        ViewBag.Deportistas = BD.ListarDeportistasD(idDeporte);
+        return View("DetalleDeporte");
+    }
+
+    public IActionResult VerDetallePais(int idPais)
+    {
+        ViewBag.Paies = BD.VerInfoPais(idPais);
+        ViewBag.Deportistas = BD.ListarDeportistasP(idPais);
+        return View("DetallePais");
+    }
+
+    public IActionResult VerDetalleDeportista(int idDeportista)
+    {
+        ViewBag.Deportista = BD.VerInfoDeportista(idDeportista);
+        return View("DetalleDeportista");
+    }
+
+    public IActionResult AgregarDeportista()
+    {
+        ViewBag.PaisesList = BD.ListarPaises();
+        ViewBag.PaisesList = BD.ListarDeportes();
+        return View();
+    }
+
+    [HttpPost] public IActionResult GuardarDeportista(Deportista dep)
+    {
+        BD.AgregarDeportista(dep);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult EliminarDeportista(int idCandidato)
+    {
+        BD.EliminarDeportista(idCandidato);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Creditos()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
--- Models/BD.cs
+++ Models/BD.cs
@@ -3,7 +3,38 @@ using Dapper;
 using System.Data.SqlClient;
 
 public static class BD{
-    static void AgregarDeportista(Deportista dep){
-        
+    public static void AgregarDeportista(Deportista dep){
+
+    }
+    public static void EliminarDeportista(int idDeportista){
+
+    }
+    public static string[] VerInfoDeporte(int idDeporte){
+
+        return null;
+    }
+    public static string[] VerInfoPais(int idPais){
+
+        return null;
+    }
+    public static string[] VerInfoDeportista(int idDeportista){
+
+        return null;
+    }
+    public static string[] ListarPaises(){
+
+        return null;
+    }
+    public static string[] ListarDeportistasD(int idDeporte){
+
+        return null;
+    }
+    public static string[] ListarDeportistasP(int idPais){
+
+        return null;
+    }
+    public static string[] ListarDeportes(){
+
+        return null;
     }
 }
