6df5dbcf45d4eeddf9cf4f87b857d3598177b43f
Android Studio <devandroid@ort>
2024-08-15 13:35:08 -0300

    Updated bootstrap

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const html = document.querySelector("#tema");
+const htmlColorMode = document.querySelector(".site-theme");
+html.setAttribute("data-bs-theme", localStorage.getItem("tema"));
+document.getElementById("themeSelector").src= localStorage.getItem("img");
+
+htmlColorMode.addEventListener("click", () => {
+    if(html.getAttribute("data-bs-theme") === "dark"){
+        localStorage.setItem("tema","light");
+        localStorage.setItem("img","/imgs/luna.png");
+    }
+    else{
+        localStorage.setItem("tema","dark");
+        localStorage.setItem("img","/imgs/sol.png");
+    }
+    html.setAttribute("data-bs-theme",localStorage.getItem("tema"));
+    document.getElementById("themeSelector").src= localStorage.getItem("img");
+})
