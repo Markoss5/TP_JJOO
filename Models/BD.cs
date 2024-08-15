2565fb22cc8bcd5ceb55fda5fa483d48fe16268f
Iamatoast <neuyv@protonmail.com>
2024-08-15 15:18:48 -0300

    BD structure

--- Models/BD.cs
+++ Models/BD.cs
@@ -3,38 +3,82 @@ using Dapper;
 using System.Data.SqlClient;
 
 public static class BD{
+    static string _connectionString = null;
     public static void AgregarDeportista(Deportista dep){
-
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            db.Execute(sql, new {IdDeportista = dep.IdDeportista, Apellido = dep.Apellido, Nombre = dep.Nombre, FechaNacimiento = dep.FechaNacimiento, Foto = dep.Foto, IdPais = dep.IdPais, IdDeporte = dep.IdDeporte});
+        }
     }
     public static void EliminarDeportista(int idDeportista){
-
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            db.Execute(sql, new { IdDeportista = idDeportista });
+        }
     }
-    public static string[] VerInfoDeporte(int idDeporte){
-
-        return null;
+    public static Deporte VerInfoDeporte(int idDeporte){
+        Deporte deporte;
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            deporte = db.QueryFirstOrDefault<Deporte>(sql, new { IdDeporte = idDeporte });
+        }
+        return deporte;
     }
-    public static string[] VerInfoPais(int idPais){
-
-        return null;
+    public static Pais VerInfoPais(int idPais){
+        Pais pais;
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            pais = db.QueryFirstOrDefault<Pais>(sql, new { IdPais = idPais });
+        }
+        return pais;
     }
-    public static string[] VerInfoDeportista(int idDeportista){
-
-        return null;
+    public static Deportista VerInfoDeportista(int idDeportista){
+        Deportista deportista;
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            deportista = db.QueryFirstOrDefault<Deportista>(sql, new { IdDeportista = idDeportista });
+        }
+        return deportista;
     }
-    public static string[] ListarPaises(){
-
-        return null;
+    public static List<Pais> ListarPaises(){
+        List<Pais> paises;
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            paises = db.Query<Pais>(sql).ToList();
+        }
+        return paises;
     }
-    public static string[] ListarDeportistasD(int idDeporte){
-
-        return null;
+    public static List<Deportista> ListarDeportistasD(int idDeporte){
+        List<Deportista> deportistas;
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            deportistas = db.Query<Deportista>(sql, new { IdDeporte = idDeporte }).ToList();
+        }
+        return deportistas;
     }
-    public static string[] ListarDeportistasP(int idPais){
-
-        return null;
+    public static List<Deportista> ListarDeportistasP(int idPais){
+        List<Deportista> deportistas;
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            deportistas = db.Query<Deportista>(sql, new { IdPais = idPais }).ToList();
+        }
+        return deportistas;
     }
-    public static string[] ListarDeportes(){
-
-        return null;
+    public static List<Deporte> ListarDeportes(){
+        List<Deporte> deportistas;
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "name";
+            deportistas = db.Query<Deporte>(sql).ToList();
+        }
+        return deportistas;
     }
 }
